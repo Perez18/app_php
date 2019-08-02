0d99255f151b7a9a75fdb54a02810836f1d09879
Perez18 <46699443+Perez18@users.noreply.github.com>
2019-08-02 14:20:46 -0500

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,14 +6,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/a4eb31a93c.js"></script>
-    <title>APP</title>
+    <link href="../static/estilo.css">
+    <link rel="shortcut icon" href="includes/static/favicon.ico" type="image/x-icon">
+    <title>GLPI- Amortizacion</title>
 </head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Amortizacion</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-<body>
-<nav class="navbar navbar-dark bg-dark">
-<a href="index.php" class=" btn btn-dark"> <h5> APP_HPH</h5></a>
-
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+        <a class="nav-link" href="index.php">Equipos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="software.php">Sofware</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="recintos.php" tabindex="-1" >Recintos</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Global
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="valort_global.php">Valor Total Global</a>
+          <a class="dropdown-item" href="valorN_global.php">Valor Total Neto Global</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" target="_blank" href="../glpi/front/report.infocom.php">Informe en GLPI</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+      <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
 </nav>
 
-</div>
+   
+
+
+
+
+
+
+<body>
